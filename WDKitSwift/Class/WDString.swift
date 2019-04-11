ae6fe0ca69034260449b4a52739de30ad8f89d83
何伟东 <654500619@qq.com>
2019-04-11 11:27:41 +0800

    提交

--- WDKitSwift/Class/WDString.swift
+++ WDKitSwift/Class/WDString.swift
@@ -26,4 +26,35 @@ public extension String {
         free(result)
         return String(format: hash as String)
     }
+    
+    // base64编码
+    func base64Encode()->String?{
+        if let data = self.data(using: .utf8) {
+            return data.base64EncodedString()
+        }
+        return nil
+    }
+    // base64解码
+    func base64Decode() -> String? {
+        if let data = Data(base64Encoded: self) {
+            return String(data: data, encoding: .utf8)
+        }
+        return nil
+    }
+    //哈希值
+    func sha1() -> String {
+        let data = self.data(using: String.Encoding.utf8, allowLossyConversion: true)!
+        let uint8Ptr = UnsafeMutablePointer<UInt8>.allocate(capacity: data.count)
+        //uint8Ptr.initialize(from: data) //<-copying the data
+        let rawPtr = UnsafeRawPointer(uint8Ptr)
+        var digest = [UInt8](repeating:0,count:Int(CC_SHA1_DIGEST_LENGTH))
+        
+        CC_SHA1(rawPtr, CC_LONG(data.count), &digest)
+        let output = NSMutableString(capacity: Int(CC_SHA1_DIGEST_LENGTH))
+        for byte in digest{
+            output.appendFormat("%02x", byte)
+        }
+        return output as String
+    }
+    
 }
